c37df2ed11db9260a5d411e347547bc9b8335b66
Terry Shenker <tshenker@gmail.com>
2019-02-07 10:15:46 +0900

    added js folder and nav styling

---
+++ js/script.js
@@ -0,0 +1,48 @@
+$(document).ready(function(){
+
+    $('#item1').click(function(){
+        $('#item1').addClass("active");
+        $('#item2').removeClass("active");
+        $('#item3').removeClass("active");
+    });
+
+    $('#item2').click(function(){
+        $('#item2').addClass("active");
+        $('#item1').removeClass("active");
+        $('#item3').removeClass("active");
+    });
+
+    $('#item3').click(function(){
+        $('#item3').addClass("active");
+        $('#item1').removeClass("active");
+        $('#item2').removeClass("active");
+    });
+
+    // initialize the accordion
+    $('.ui.accordion').accordion();
+
+    // initializing the modal
+    $("#langexbot_modal").click(function(){
+        $(".langexbot_modal").modal('show');
+    });
+    $(".langexbot_modal").modal({
+        closable: true
+    });
+
+    $("#npad_modal").click(function(){
+        $(".npad_modal").modal('show');
+    });
+    $(".npad_modal").modal({
+        closable: true
+    });
+
+    $("#marvel_modal").click(function(){
+        $(".marvel_modal").modal('show');
+    });
+    $(".marvel_modal").modal({
+        closable: true
+    });
+
+    // scrollspy
+        
+});
